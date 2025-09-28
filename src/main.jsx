b24c2897d1efd952979d5dae1e9c041682c19f93
mishra-ujjwal <ujjwalmishra9717@gmail.com>
2025-09-29 00:18:05 +0530

    deploylink

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,8 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { BrowserRouter, RouterProvider } from 'react-router-dom'
-import router from './router.jsx'
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import { RouterProvider } from 'react-router-dom';
+import router from './router.jsx';
 
 createRoot(document.getElementById('root')).render(
-   <RouterProvider router={router}>
-     <App/>
-   </RouterProvider>
-)
+  <RouterProvider router={router} />
+);
--- src/router.jsx
+++ src/router.jsx
@@ -1,38 +1,19 @@
-import { createBrowserRouter } from "react-router-dom";
+import { createHashRouter } from "react-router-dom";
 import App from "./App";
 import StudySyncDashboard from "./pages/StudySyncDashboard";
 import ChoosePath from "./components/ChoosePath";
 import CollegeLearningDashboard from "./components/College";
 
-const router = createBrowserRouter([
-    {
-        path:"/",
-        element:<App/>, 
-    },{
-        path:"class-9",
-        element:<StudySyncDashboard/>
-    },{
-        path:"class-10",
-        element:<StudySyncDashboard/>
-    },{
-        path:"class-11",
-        element:<StudySyncDashboard/>
-    },{
-        path:"class-12",
-        element:<StudySyncDashboard/>
-    },{
-        path:"JEE",
-        element:<StudySyncDashboard/>
-    },{
-        path:"NEET",
-        element:<StudySyncDashboard/>
-    },{
-        path:"B.Tech/College",
-        element:<CollegeLearningDashboard/>
-    }
-    ,{
-        path:"choose-path",
-        element:<ChoosePath/>
-    }
-])
+const router = createHashRouter([
+  { path: "/", element: <App /> },
+  { path: "class-9", element: <StudySyncDashboard /> },
+  { path: "class-10", element: <StudySyncDashboard /> },
+  { path: "class-11", element: <StudySyncDashboard /> },
+  { path: "class-12", element: <StudySyncDashboard /> },
+  { path: "JEE", element: <StudySyncDashboard /> },
+  { path: "NEET", element: <StudySyncDashboard /> },
+  { path: "B.Tech/College", element: <CollegeLearningDashboard /> },
+  { path: "choose-path", element: <ChoosePath /> },
+]);
+
 export default router;
